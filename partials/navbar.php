9473e65c4ff8eef6e17742544330d269e54f9b43
Mart <you@example.com>
2020-11-11 15:08:31 +0100

    ⭐ - cnavbar

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,37 +1,39 @@
 <nav class="flex flex-col">
 
-    <div class="nav flex flex-row p-2 justify-between">
+    <div class="nav flex flex-row p-2 justify-between lg:grid grid-cols-3">
         
-        <div class="flex-grow">
-        <object class="logo" id="logo" type="image/svg+xml" data="public/img/logo.svg"></object>
+        <div class="">
+            <object class="logo" id="logo" type="image/svg+xml" data="public/img/logo.svg"></object>
         </div>
 
-        <div class="bg-gray-100 p-2 rounded">
+        <div class="bg-gray-100 p-2 rounded flex-grow lg:flex-grow-0 max-w-full mx-8 lg:mx-0">
             <input class="text-gray-800" type="text" placeholder="Zoeken!" id="zoek" type="text" name="zoek" value="">
         </div>
     
-    <div class="items-center flex flex-row flex-grow">
-        <div class="p-2 text-gray-200">
-            <a>English</a>
-        </div>
+        <div class="items-center flex flex-row justify-end">
+            <div class="p-2 text-gray-200">
+                <a>English</a>
+            </div>
 
-        <div class="p-2 text-gray-200">
-            <a>Inloggen</a>
-        </div>
+            <div class="p-2 text-gray-200">
+                <a>Inloggen</a>
+            </div>
 
-        <div class="pl-2 text-gray-200">
-            <a><ion-icon name="cart-outline" size="large"></ion-icon></a>
-        </div>
+            <div class="px-2 pt-1 text-gray-200">
+                <a><ion-icon name="cart-outline" size="large"></ion-icon></a>
+            </div>
         </div>
     </div>
 
-    <div class="flex flex-row">
-        <div class="">
-            <a>Eten</a>
-        </div>
+    <div class="categories">
+        <div class="container mx-auto flex flex-row">
+            <div class="text-gray-200 p-1 px-2">
+                <a>Eten</a>
+            </div>
 
-        <div class="">
-            <a>Kleren</a>
+            <div class="text-gray-200 p-1 px-2">
+                <a>Kleren</a>
+            </div>
         </div>
     </div>
 
@@ -40,5 +42,9 @@
 <style>
 .nav{
     background-color: #1E0253;
+}
+
+.categories{
+    background-color: #023153;
 }
 </style>
