897cf169bd94a0a098c2ade4ed75145709f9aabd
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-12 16:38:50 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,27 +1,94 @@
 <section class="container">
     <p>Aangeraden producten</p>
     <div class="flex justify-between bg-gray-200">
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 1</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 2</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 3</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 4</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 5</div>
-    </div><br>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/580b57fbd9996e24bc43bf55.png"/>
+            </a>
+            <p>Product 1</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/580b57fbd9996e24bc43bf55.png"/>
+            </a>
+            <p>Product 2</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/580b57fbd9996e24bc43bf55.png"/>
+            </a>
+            <p>Product 3</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/580b57fbd9996e24bc43bf55.png"/>
+            </a>
+            <p>Product 4</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/580b57fbd9996e24bc43bf55.png"/>
+            </a>
+            <p>Product 5</p>
+        </div>
+    </div>
+    <br>
 
     <p>Aangeraden categorieën</p>
     <div class="flex justify-between bg-gray-200">
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 1</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 2</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 3</div>
-    </div><br>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-64 w-64" href="">
+                <img src="public/ProductIMGHighRes/hoodie.png"/>
+            </a>
+            <p>Product 1</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-64 w-64" href="">
+                <img src="public/ProductIMGHighRes/hoodie.png"/>
+            </a>
+            <p>Product 2</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-64 w-64" href="">
+                <img src="public/ProductIMGHighRes/hoodie.png"/>
+            </a>
+            <p>Product 3</p>
+        </div>
+    </div>
+    <br>
 
     <p>Producten</p>
-    <div class="flex justify-evenly bg-gray-200">
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 1</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 2</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 3</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 4</div>
-        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 5</div>
+    <div class="flex justify-between bg-gray-200">
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/mug.png"/>
+            </a>
+            <p>Product 1</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/mug.png"/>
+            </a>
+            <p>Product 2</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/mug.png"/>
+            </a>
+            <p>Product 3</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/mug.png"/>
+            </a>
+            <p>Product 4</p>
+        </div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">
+            <a class="h-32 w-32" href="">
+                <img src="public/ProductIMGHighRes/mug.png"/>
+            </a>
+            <p>Product 5</p>
+        </div>
     </div>
 
 
