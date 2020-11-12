e6a7f3013d73129f1da013fdd6ff641fc12d1663
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-12 14:04:35 +0100

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,33 @@
-<section><p>footer</p></section>
+<div class="nav w-full py-2">
+
+    <div class="justify-center text-center py-1">
+        <ion-icon name="logo-facebook"></ion-icon>
+        <ion-icon name="logo-twitter"></ion-icon>
+        <ion-icon name="logo-youtube"></ion-icon>
+        <ion-icon name="logo-instagram"></ion-icon>
+    </div>
+
+    <div class="justify-center text-center py-1 h-16 w-16">
+            <img src="https://www.ideal.nl/img/logo/ideal-logo.svg"/>
+    </div>
+
+    <div class="justify-center text-center py-1">
+        <p>
+            <a href="contact">Contact</a> |
+            <a href="tos">Algemene voorwaarden</a> |
+            <a href="privacy">Privacy</a> |
+            <a href="dutch">Nederlands</a>
+        </p>
+    </div>
+
+    <div class="justify-center text-center py-1">
+        <p>© 2020 - 2021 NerdyGadgets.nl</p>
+    </div>
+
+</div>
+
+<style>
+    .nav {
+        background-color: #1E0253;
+        color: #ffffff;
+</style>
