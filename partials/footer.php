af69fe2ce06bfce397fec8cffe2b284281e6b658
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-12 14:45:00 +0100

    🖥️ - Footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,14 +1,22 @@
-<div class="nav w-full my-2">
+<div class="footer text-white w-full py-2">
 
-    <div class="flex justify-center text-center py-2 gap-2">
-        <ion-icon name="logo-facebook"></ion-icon>
+    <div class="flex justify-center text-center py-2 gap-3">
+        <a href="https://facebook.com" target="_blank">
+            <ion-icon name="logo-facebook"></ion-icon>
+        </a>
+        <a href="https://twitter.com" target="_blank">
         <ion-icon name="logo-twitter"></ion-icon>
+        </a>
+        <a href="https://youtube.com" target="_blank">
         <ion-icon name="logo-youtube"></ion-icon>
+        </a>
+        <a href="https://instagram.com" target="_blank">
         <ion-icon name="logo-instagram"></ion-icon>
+        </a>
     </div>
 
     <div class="flex justify-center py-2 h-16">
-        <img src="https://www.ideal.nl/img/logo/ideal-logo.svg" />
+        <img src="https://www.ideal.nl/img/logo/ideal-logo.svg"/>
     </div>
 
     <div class="text-center py-1">
@@ -27,8 +35,7 @@
 </div>
 
 <style>
-    .nav {
+    .footer {
         background-color: #1E0253;
-        color: #ffffff;
     }
 </style>
