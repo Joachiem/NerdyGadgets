78d30a2f10a264d7858bb45eb421f641e9c4c988
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-12 16:22:55 +0100

    testing grids

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,28 @@
 <section class="container">
-    <h1>index</h1>
+    <p>Aangeraden producten</p>
+    <div class="flex justify-between bg-gray-200">
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 1</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 2</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 3</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 4</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 5</div>
+    </div><br>
+
+    <p>Aangeraden categorieën</p>
+    <div class="flex justify-between bg-gray-200">
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 1</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 2</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 3</div>
+    </div><br>
+
+    <p>Producten</p>
+    <div class="flex justify-evenly bg-gray-200">
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 1</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 2</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 3</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 4</div>
+        <div class="text-gray-700 bg-gray-400 flex justify-center items-center px-4 py-2">Product 5</div>
+    </div>
+
+
 </section>
