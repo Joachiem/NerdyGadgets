336b82bb81aa6393a6735c02be56fbc47c3f1931
Mart <you@example.com>
2020-11-13 15:09:42 +0100

    delete querys

--- partials/navbar.php
+++ partials/navbar.php
@@ -2,7 +2,7 @@
     <div class="nav flex flex-row p-2 justify-between lg:grid grid-cols-3">
 
         <a href="/" draggable="false">
-            <img class="logo" id="logo" draggable="false" src="<?php print "http://" . $_SERVER['HTTP_HOST'] . "/public/img/logo.svg" ?>" />
+            <img class="logo" id="logo" draggable="false" src="/public/img/logo.svg" />
         </a>
 
         <div class="flex align-center bg-gray-100 rounded flex-grow lg:flex-grow-0 max-w-full mx-8 lg:mx-0">
