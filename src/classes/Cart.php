ddc444cfe88b0d9db534e7f290d237250f9e80e2
Mart <you@example.com>
2020-11-15 17:13:52 +0100

    error fix

--- src/classes/Cart.php
+++ src/classes/Cart.php
@@ -7,26 +7,30 @@ class Cart
      */
     public static function index()
     {
-        $products = $_SESSION['cart'];
-
         $arg = [];
+        $images = '';
 
-        foreach ($products as $id => $qty) {
+        if (isset($_SESSION['cart'])) {
+            $products = $_SESSION['cart'];
+
+            $arg = [];
+
+            foreach ($products as $id => $qty) {
 
 
-            try {
-                $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
-                mysqli_set_charset($Connection, 'latin1');
-                $DatabaseAvailable = true;
-            } catch (mysqli_sql_exception $e) {
-                $DatabaseAvailable = false;
-            }
-            if (!$DatabaseAvailable) {
-                print '<h2>Website wordt op dit moment onderhouden.</h2>';
-                die();
-            }
+                try {
+                    $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
+                    mysqli_set_charset($Connection, 'latin1');
+                    $DatabaseAvailable = true;
+                } catch (mysqli_sql_exception $e) {
+                    $DatabaseAvailable = false;
+                }
+                if (!$DatabaseAvailable) {
+                    print '<h2>Website wordt op dit moment onderhouden.</h2>';
+                    die();
+                }
 
-            $Query = "SELECT SI.StockItemID,
+                $Query = "SELECT SI.StockItemID,
         (RecommendedRetailPrice*(1+(TaxRate/100))) AS SellPrice, 
         StockItemName,
         CONCAT('Voorraad: ',QuantityOnHand)AS QuantityOnHand,
@@ -41,37 +45,37 @@ class Cart
         GROUP BY StockItemID";
 
 
-            $ShowStockLevel = 1000;
-            $Statement = mysqli_prepare($Connection, $Query);
-            mysqli_stmt_bind_param($Statement, "i", $id);
-            mysqli_stmt_execute($Statement);
-            $ReturnableResult = mysqli_stmt_get_result($Statement);
-            if ($ReturnableResult && mysqli_num_rows($ReturnableResult) == 1) {
-                $Result = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC)[0];
-            } else {
-                $Result = null;
+                $ShowStockLevel = 1000;
+                $Statement = mysqli_prepare($Connection, $Query);
+                mysqli_stmt_bind_param($Statement, "i", $id);
+                mysqli_stmt_execute($Statement);
+                $ReturnableResult = mysqli_stmt_get_result($Statement);
+                if ($ReturnableResult && mysqli_num_rows($ReturnableResult) == 1) {
+                    $Result = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC)[0];
+                } else {
+                    $Result = null;
+                }
+
+                //Get Images
+                $Query = "SELECT ImagePath FROM stockitemimages WHERE StockItemID = ?";
+
+                $Statement = mysqli_prepare($Connection, $Query);
+                mysqli_stmt_bind_param($Statement, "i", $id);
+                mysqli_stmt_execute($Statement);
+                $image_result = mysqli_stmt_get_result($Statement);
+                $image_result = mysqli_fetch_all($image_result, MYSQLI_ASSOC);
+
+
+                if ($image_result) {
+                    $images = $image_result;
+                }
+
+                $arg[$id] = [
+                    'qty' => $qty,
+                    'product' => $Result,
+                    'images' => $images
+                ];
             }
-
-            //Get Images
-            $Query = "SELECT ImagePath FROM stockitemimages WHERE StockItemID = ?";
-
-            $Statement = mysqli_prepare($Connection, $Query);
-            mysqli_stmt_bind_param($Statement, "i", $id);
-            mysqli_stmt_execute($Statement);
-            $image_result = mysqli_stmt_get_result($Statement);
-            $image_result = mysqli_fetch_all($image_result, MYSQLI_ASSOC);
-
-
-            $images = '';
-            if ($image_result) {
-                $images = $image_result;
-            }
-
-            $arg[$id] = [
-                'qty' => $qty,
-                'product' => $Result,
-                'images' => $images
-            ];
         }
 
         View::show('cart/index', $arg);
