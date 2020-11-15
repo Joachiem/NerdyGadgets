0bb6b6d0284a74a7b07d1f092d98e63fd16d4774
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-15 12:42:44 +0100

    shopping cart link

--- src/functions/cart.php
+++ src/functions/cart.php
@@ -2,7 +2,8 @@
 
 function cart()
 {
-    $arg = ['dffd' => 'sdfjts', 'sddd' => 'jttjt', 'sdf' => 'gfggf'];
+    // Array = 'artikel ID' => 'aantal in winkelmandje'
+    $arg = ['3' => '1', '14' => '6', '69' => '2', '111' => '3'];
 
     
     View::show('cart/index', $arg);
