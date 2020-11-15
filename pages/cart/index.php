01707c49102ddce6c5feef6bbb7f760b11156a5f
camiël vd Niet <35525009+camvdniet@users.noreply.github.com>
2020-11-15 16:25:17 +0100

    Update index.php

--- pages/cart/index.php
+++ pages/cart/index.php
@@ -15,18 +15,74 @@
                     </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($arg as $product_id => $qty) { ?>
+                <?php foreach ($arg as $product_id => $qty) { 
+
+                try {
+                    $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
+                    mysqli_set_charset($Connection, 'latin1');
+                    $DatabaseAvailable = true;
+                } catch (mysqli_sql_exception $e) {
+                    $DatabaseAvailable = false;
+                }
+                if (!$DatabaseAvailable) {
+                    print '<h2>Website wordt op dit moment onderhouden.</h2>';
+                    die();
+                }
+        
+                $Query = "SELECT SI.StockItemID,
+                (RecommendedRetailPrice*(1+(TaxRate/100))) AS SellPrice, 
+                StockItemName,
+                CONCAT('Voorraad: ',QuantityOnHand)AS QuantityOnHand,
+                SearchDetails, 
+                (CASE WHEN (RecommendedRetailPrice*(1+(TaxRate/100))) > 50 THEN 0 ELSE 6.95 END) AS SendCosts, MarketingComments, CustomFields, SI.Video,
+                (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath   
+                FROM stockitems SI 
+                JOIN stockitemholdings SIH USING(stockitemid)
+                JOIN stockitemstockgroups ON SI.StockItemID = stockitemstockgroups.StockItemID
+                JOIN stockgroups USING(StockGroupID)
+                WHERE SI.stockitemid = ?
+                GROUP BY StockItemID";
+        
+                $ShowStockLevel = 1000;
+                $Statement = mysqli_prepare($Connection, $Query);
+                mysqli_stmt_bind_param($Statement, "i", $product_id);
+                mysqli_stmt_execute($Statement);
+                $ReturnableResult = mysqli_stmt_get_result($Statement);
+                if ($ReturnableResult && mysqli_num_rows($ReturnableResult) == 1) {
+                    $Result = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC)[0];
+                } else {
+                    $Result = null;
+                }
+        
+                //Get Images
+                $Query = "
+                    SELECT ImagePath
+                    FROM stockitemimages 
+                    WHERE StockItemID = ?";
+        
+                $Statement = mysqli_prepare($Connection, $Query);
+                mysqli_stmt_bind_param($Statement, "i", $product_id);
+                mysqli_stmt_execute($Statement);
+                $R = mysqli_stmt_get_result($Statement);
+                $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
+       
+                if ($R) {
+                    $Images = $R;
+                }
+            
+                ?>
+                    
                     <tr>
                         <td class="hidden pb-4 md:table-cell">
                             <a href="#">
-                                <img src="https://limg.app/i/Calm-Cormorant-Catholic-Pinball-Blaster-yM4oub.jpeg" class="w-20 rounded" alt="Thumbnail">
+                                <img src="/public/StockItemIMG/<?php print $Images[0]['ImagePath']; ?>" class="w-20 rounded" alt="Thumbnail">
                             </a>
                         </td>
                         <td>
                             <a href="#">
-                                <p class="mb-2 md:ml-4">Earphone</p>
+                                <p class="mb-2 md:ml-4"><?php print $Result['StockItemName']; ?></p>
                                 <form action="" method="POST">
-                                    <button type="submit" class="text-gray-700 md:ml-4">
+                                    <button type="submit" value="<?php print($product_id)?>" class="text-gray-700 md:ml-4">
                                         <small>(Verwijder product)</small>
                                     </button>
                                 </form>
@@ -47,7 +103,7 @@
                         </td>
                         <td class="hidden text-right md:table-cell">
                             <span class="text-sm lg:text-base font-medium">
-                                €10.00
+                                <?php print sprintf("€ %.2f", $Result['SellPrice']); ?>
                             </span>
                         </td>
                         <td class="text-right">
