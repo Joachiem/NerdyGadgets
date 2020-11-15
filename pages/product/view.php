d2a323b7d031c01b9e3b14638f22d8f807ab8599
Mart <you@example.com>
2020-11-15 13:33:55 +0100

    product page gone css

--- pages/product/view.php
+++ pages/product/view.php
@@ -1,11 +1,3 @@
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product</title>
-
-</head>
 <?php
 $Query = " 
         SELECT SI.StockItemID, 
@@ -48,150 +40,144 @@ if ($R) {
     $Images = $R;
 }
 
-    if ($Result != null) {
-    ?>
-            <body>
-                <div class="centercontainer">
-                    <div class="prodname">
-                        <h1><?php print $Result['StockItemName']; ?></h1>
+if ($Result != null) {
+?>
+
+
+
+    <div class="centercontainer">
+        <div class="prodname">
+            <h1><?php print $Result['StockItemName']; ?></h1>
+        </div>
+        <div class="prodprev">
+            <div class="slideshow-container">
+                <?php for ($i = 0; $i < count($Images); $i++) {
+                ?>
+                    <div class="mySlides fade" style="display: block;">
+                        <img src="/public/StockItemIMG/<?php print $Images[$i]['ImagePath'] ?>">
                     </div>
-                    <div class="prodprev">
-                        <div class="slideshow-container">
-                            <?php for ($i = 0; $i < count($Images); $i++) {
-                            ?>
-                                <div class="mySlides fade" style="display: block;">
-                                    <img src="/public/StockItemIMG/<?php print $Images[$i]['ImagePath'] ?>">
-                                </div>
-                            <?php } ?>
-
-                            <?php if (count($Images) >= 2) { ?>
-                                <a class="prev" onclick="plusSlides(-1)">❮</a>
-                                <a class="next" onclick="plusSlides(1)">❯</a>
-
-                        </div>
-                        <br>
-
-                        <div style="text-align:center">
-                            <?php for ($i = 0; $i < count($Images); $i++) {
-                            ?>
-                                <span class="miniprev" onclick="currentSlide(<?php print($i + 1) ?>)"><img src="/public/StockItemIMG/<?php print $Images[$i]['ImagePath'] ?>"></span>
-                        <?php }
-                            } ?>
-                        </div>
-                    </div>
-                    <div class="title">
-                        <h2><?php print sprintf("€ %.2f", $Result['SellPrice']); ?></h2>
-                        <ul>
-                            <li>verzending binnen 2 werkdagen</li>
-                            <li>verzending boven 20 euro gratis</li>
-                            <li>2 jaar garantie</li>
-                        </ul>
-                        <a href="winkelmandje.php">
-                            <div class="buybutton">In winkelmandje</div>
-                        </a>
-                    </div>
-                    <div class="infobox">
-                        <h2>Beschrijving</h2>
-                        <p><?php print $Result['SearchDetails']; ?></p>
-                    </div>
-                    <div class="infobox">
-                        <h2>Specificaties</h2>
-                        <?php
-                        $CustomFields = json_decode($Result['CustomFields'], true);
-                        if (is_array($CustomFields)) { ?>
-                            <table>
-                                <thead>
-                                    <th>Naam</th>
-                                    <th>Data</th>
-                                </thead>
-                                <?php
-                                foreach ($CustomFields as $SpecName => $SpecText) { ?>
-                                    <tr>
-                                        <td>
-                                            <?php print $SpecName; ?>
-                                        </td>
-                                        <td>
-                                            <?php
-                                            if (is_array($SpecText)) {
-                                                foreach ($SpecText as $SubText) {
-                                                    print $SubText . " ";
-                                                }
-                                            } else {
-                                                print $SpecText;
-                                            }
-                                            ?>
-                                        </td>
-                                    </tr>
-
-                                <?php } ?>
-
-                            </table>
-
-                        <?php } else { ?>
-
-                            <p><?php print $Result['CustomFields']; ?>.</p>
-
-                        <?php } ?>
-
-                    </div>
-
-                <?php } else { ?>
-
-                    <h2 id="ProductNotFound">Het opgevraagde product is niet gevonden.</h2>
-
                 <?php } ?>
 
-                </div>
+                <?php if (count($Images) >= 2) { ?>
+                    <a class="prev" onclick="plusSlides(-1)">❮</a>
+                    <a class="next" onclick="plusSlides(1)">❯</a>
+
+            </div>
+
+            <div style="text-align:center">
+                <?php for ($i = 0; $i < count($Images); $i++) {
+                ?>
+                    <span class="miniprev" onclick="currentSlide(<?php print($i + 1) ?>)"><img src="/public/StockItemIMG/<?php print $Images[$i]['ImagePath'] ?>"></span>
+            <?php }
+                } ?>
+            </div>
+        </div>
+        <div class="title">
+            <h2><?php print sprintf("€ %.2f", $Result['SellPrice']); ?></h2>
+            <ul>
+                <li>verzending binnen 2 werkdagen</li>
+                <li>verzending boven 20 euro gratis</li>
+                <li>2 jaar garantie</li>
+            </ul>
+            <a href="winkelmandje.php">
+                <div class="buybutton">In winkelmandje</div>
+            </a>
+        </div>
+        <div class="infobox">
+            <h2>Beschrijving</h2>
+            <p><?php print $Result['SearchDetails']; ?></p>
+        </div>
+        <div class="infobox">
+            <h2>Specificaties</h2>
+            <?php
+            $CustomFields = json_decode($Result['CustomFields'], true);
+            if (is_array($CustomFields)) { ?>
+                <table>
+                    <thead>
+                        <th>Naam</th>
+                        <th>Data</th>
+                    </thead>
+                    <?php
+                    foreach ($CustomFields as $SpecName => $SpecText) { ?>
+                        <tr>
+                            <td>
+                                <?php print $SpecName; ?>
+                            </td>
+                            <td>
+                                <?php
+                                if (is_array($SpecText)) {
+                                    foreach ($SpecText as $SubText) {
+                                        print $SubText . " ";
+                                    }
+                                } else {
+                                    print $SpecText;
+                                }
+                                ?>
+                            </td>
+                        </tr>
+
+                    <?php } ?>
+
+                </table>
+
+            <?php } else { ?>
+
+                <p><?php print $Result['CustomFields']; ?>.</p>
+
+            <?php } ?>
 
         </div>
-</div>
+
+    <?php } else { ?>
+
+        <h2 id="ProductNotFound">Het opgevraagde product is niet gevonden.</h2>
+
+    <?php } ?>
+
+    </div>
 
 
-<script>
-    var slideIndex = 1;
-    showSlides(slideIndex);
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+    <script>
+        var slideIndex = 1;
+        showSlides(slideIndex);
 
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {
-            slideIndex = 1
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
         }
-        if (n < 1) {
-            slideIndex = slides.length
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
         }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = slides.length
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
         }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-    }
-</script>
+        
 
-<script>
-    (() => {
-        const addToCartBtn = document.querySelector('#add-to-card-btn')
+        (() => {
+            const addToCartBtn = document.querySelector('#add-to-card-btn')
 
-        addToCartBtn.addEventListener("click", () => {
-            let request = new XMLHttpRequest()
-            request.open('POST', '/products/add?id=1')
-            request.send()
-        })
-    })()
-</script>
-
-</body>
-
-</html>
+            addToCartBtn.addEventListener("click", () => {
+                let request = new XMLHttpRequest()
+                request.open('POST', '/products/add?id=1')
+                request.send()
+            })
+        })()
+    </script>
