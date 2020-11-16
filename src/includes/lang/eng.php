09f98d7097bc90dcb6af52e68a742d72cc6a6cf4
MarkGortemaker <70703393+MarkGortemaker@users.noreply.github.com>
2020-11-16 15:52:43 +0100

    Nav en Prod Index

--- src/includes/lang/eng.php
+++ src/includes/lang/eng.php
@@ -4,7 +4,24 @@ $GLOBALS['t'] = [
     'home-page-title' => 'HELLO',
     'lastname' => 'Lastname',
     'firstname' => 'firstname',
+    //Navbar
+    'navbar-products' => 'Products',
+    'navbar-categories' => 'Categories',
+    'navbar-language' => 'dutch',
+    'navbar-language-title' => 'Nederlands',
+    'navbar-log-in' => 'Log in',
     '' => '',
     '' => '',
-    '' => '',
+
+    //product-index
+    'Product-index-search' => 'Search',
+    'Product-index-num-product' => 'Number of Products on page',
+    'Product-index-sort' => 'Sort',
+    'Product-index-sort-price-asc' => 'Price ascending',
+    'Product-index-sort-price-desc' => 'Price descending',
+    'Product-index-sort-name-asc' => 'Name acsending',
+    'Product-index-sort-name-desc' => 'Name descnding',
+    'Product-index-vat' => 'including VAT',
+    'Product-index-product-num' => 'Article Number:',
+    'Product-index-not-found' => 'Yarr, er zijn geen resultaten gevonden',
 ];
