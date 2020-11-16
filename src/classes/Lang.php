e52ffa09cae900f878d2cc8adefecb09f21db661
Mart <you@example.com>
2020-11-16 13:23:02 +0100

    llang

--- src/classes/Lang.php
+++ src/classes/Lang.php
@@ -1,10 +1,17 @@
 <?php
 class Lang
 {
-    public static function eng(){
+    public static function getLang(){
+        if(!isset($_SESSION['lang'])) $_SESSION['lang'] = 'nl';
 
+        require_once 'src/includes/lang/' . $_SESSION['lang'] . '.php';
     }
-    public static function nl(){
 
+    public static function eng(){
+        $_SESSION['lang'] = 'eng';
+    }
+
+    public static function nl(){
+        $_SESSION['lang'] = 'nl';
     }
 }
