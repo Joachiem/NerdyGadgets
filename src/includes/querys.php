704c80603891eb0bd15f90e4b11637f6d81f9ed8
Mart <you@example.com>
2020-11-16 12:47:06 +0100

    query

--- src/includes/querys.php
+++ src/includes/querys.php
@@ -1,13 +1,13 @@
 <?php
 
 $GLOBALS['q'] = [
-// test for 
+    // test for 
     'test' => 'SELECT StockGroupID, StockGroupName, ImagePath
     FROM stockgroups
     $1 
     limit ?',
 
-// get all categories that have a image
+    // get all categories that have a image
     'categories' => 'SELECT StockGroupID, StockGroupName, ImagePath
     FROM stockgroups
     WHERE StockGroupID
@@ -18,4 +18,30 @@ $GLOBALS['q'] = [
     ) 
     AND ImagePath IS NOT NULL
     ORDER BY StockGroupID DESC',
+
+// get a product
+    'product' => "SELECT SI.StockItemID,
+    (RecommendedRetailPrice*(1+(TaxRate/100))) AS SellPrice,
+    StockItemName,
+    CONCAT('Voorraad: ',QuantityOnHand) AS QuantityOnHand,
+    SearchDetails, 
+    (
+        CASE WHEN (RecommendedRetailPrice*(1+(TaxRate/100))) > 50 THEN 0 ELSE 6.95 END
+    )
+    AS SendCosts, MarketingComments, CustomFields, SI.Video,
+    (
+        SELECT ImagePath
+        FROM stockgroups
+        JOIN stockitemstockgroups USING(StockGroupID)
+        WHERE StockItemID = SI.StockItemID
+        LIMIT 1
+    ) 
+    AS BackupImagePath   
+    FROM stockitems SI 
+    JOIN stockitemholdings SIH USING(stockitemid)
+    JOIN stockitemstockgroups 
+    ON SI.StockItemID = stockitemstockgroups.StockItemID
+    JOIN stockgroups USING(StockGroupID)
+    WHERE SI.stockitemid = ?
+    GROUP BY StockItemID"
 ];
