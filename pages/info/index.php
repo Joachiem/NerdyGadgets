2e7dbd55bdd122e222eaadba301cf13c279b46c5
RachedBataineh <70702863+RachedBataineh@users.noreply.github.com>
2020-11-19 15:29:54 +0100

    contact3

--- pages/info/index.php
+++ pages/info/index.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="utf-8">
     <title>Contact form</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <main>
     <p>E-mail versturen</p>
+    <div class="p-4">
     <form class="contact-form" action="contactform.php" method="post">
-        <input type="text" name="name" placeholder="Volledige naam">
-        <input type="text" name="mail" placeholder="Jouw E-mail">
-        <input type="text" name="subject" placeholder="Onderwerp">
-        <textarea name="message" placeholder="Bericht"></textarea>
-        <button type="submit" name="submit">Versturen</button>
+        <input type="text" name="name" placeholder="Volledige naam"> <br>
+        <input type="text" name="mail" placeholder="Jouw E-mail"> <br>
+        <input type="text" name="subject" placeholder="Onderwerp"> <br>
+        <textarea name="message" placeholder="Bericht"></textarea> <br>
+        <button class="bg-red-500 hover:bg-red-700" type="submit" name="submit">Versturen</button>
 </main>
 </body>
 </html>
