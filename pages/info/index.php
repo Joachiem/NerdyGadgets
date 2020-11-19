c462670d12fcb11720b179384aa01d5162a720b1
RachedBataineh <70702863+RachedBataineh@users.noreply.github.com>
2020-11-19 15:56:06 +0100

    Update index.php

--- pages/info/index.php
+++ pages/info/index.php
@@ -8,9 +8,9 @@
     <p>E-mail versturen</p>
     <div>
         <form action="contactform.php" method="post">
-            <input class="mb-4" type="text" name="name" placeholder="Volledige naam"> <br><br>
-            <input class="mb-4" type="text" name="mail" placeholder="Jouw E-mail"> <br><br>
-            <input class="mb-4" type="text" name="subject" placeholder="Onderwerp"> <br><br>
+            <input class="mb-4" type="text" name="name" placeholder="Volledige naam"> <br>
+            <input class="mb-4" type="text" name="mail" placeholder="Jouw E-mail"> <br>
+            <input class="mb-4" type="text" name="subject" placeholder="Onderwerp"> <br>
             <textarea name="message" placeholder="Bericht"></textarea> <br>
             <button class="bg-red-500 hover:bg-red-700 p-2 rounded" type="submit" name="submit">Versturen</button>
         </form>
