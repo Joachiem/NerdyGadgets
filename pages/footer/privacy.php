a5ced830adedf9f5481513a8a63120343c1bdee0
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-20 11:34:23 +0100

    Privacy translation

--- pages/footer/privacy.php
+++ pages/footer/privacy.php
@@ -1,33 +1,33 @@
 <div class="mb-6 space-y-4">
-    <?php print $GLOBALS['t']['p']['privacy-title'] ?>
+    <?php print $GLOBALS['t']['p']['title'] ?>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-1'] ?>
+        <?php print $GLOBALS['t']['p']['1'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-2'] ?>
+        <?php print $GLOBALS['t']['p']['2'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-3'] ?>
+        <?php print $GLOBALS['t']['p']['3'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-4'] ?>
+        <?php print $GLOBALS['t']['p']['4'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-5'] ?>
+        <?php print $GLOBALS['t']['p']['5'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-6'] ?>
+        <?php print $GLOBALS['t']['p']['6'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-7'] ?>
+        <?php print $GLOBALS['t']['p']['7'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-8'] ?>
+        <?php print $GLOBALS['t']['p']['8'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-9'] ?>
+        <?php print $GLOBALS['t']['p']['9'] ?>
     </div>
     <div>
-        <?php print $GLOBALS['t']['p']['privacy-10'] ?>
+        <?php print $GLOBALS['t']['p']['10'] ?>
     </div>
 </div>
