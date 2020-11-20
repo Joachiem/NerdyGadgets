8c1276f3bfcd2ce4f3ac76d17cf23aa9d77b513b
MarkGortemaker <70703393+MarkGortemaker@users.noreply.github.com>
2020-11-20 11:25:07 +0100

    begin

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -1,1 +1,8 @@
-<h1>Hello contact</h1>
+<?php ?>
+        <h1>Contactinformatie</h1>
+
+    <div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8"">
+        <div class="border flex-1"> <p>Consument</p></div>
+        <div class="border flex-auto"> <p>Zakelijk</p></div>
+        <div class="border flex-auto"> <p>Sociale media</p></div>
+    </div>
