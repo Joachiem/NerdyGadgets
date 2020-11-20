78916cfdbbff4a83a09ac67467bbdd84fbc5e76a
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-20 11:01:07 +0100

    Privacy

--- pages/footer/privacy.php
+++ pages/footer/privacy.php
@@ -1,1 +1,33 @@
-<h1>Hello privacy</h1>
+<div class="mb-6 space-y-4">
+    <?php print $GLOBALS['t']['privacy-title'] ?>
+    <div>
+        <?php print $GLOBALS['t']['privacy-1'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-2'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-3'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-4'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-5'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-6'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-7'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-8'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-9'] ?>
+    </div>
+    <div>
+        <?php print $GLOBALS['t']['privacy-10'] ?>
+    </div>
+</div>
