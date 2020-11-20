1cceab0fa0f14027a954173cc6aa37895608e7c0
Mart-0 <s1158005@student.windesheim.nl>
2020-11-20 12:00:04 +0100

    classes

--- src/classes/Homepage.php
+++ src/classes/Homepage.php
@@ -9,11 +9,10 @@ class Homepage
     public static function index()
     {
         $arg = new stdClass();
-        $arg->products = DB::execute($GLOBALS['q']['products'], [], ['102,75,32,4,46,160']);
 
         $arg->popularProducts = DB::execute($GLOBALS['q']['popular-products'], [6]);
-
         $arg->categories = DB::execute($GLOBALS['q']['categories']);
+        $arg->products = DB::execute($GLOBALS['q']['products'], [], ['102,75,32,4,46,160']);
 
         View::show('index', $arg);
     }
