55d300871aeb6e123736ef7ca94a72438896e99a
MarkGortemaker <70703393+MarkGortemaker@users.noreply.github.com>
2020-11-20 14:15:35 +0100

    tekst

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -1,8 +1,44 @@
 <?php ?>
         <h1>Contactinformatie</h1>
 
+<<<<<<< Updated upstream
     <div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
         <div class="border flex-1"> <p>Consument</p></div>
         <div class="border flex-auto"> <p>Zakelijk</p></div>
         <div class="border flex-auto"> <p>Sociale media</p></div>
+=======
+    <div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8"">
+        <div class="border flex-1"> <p>Consument</p>
+        
+      
+	<h2>Consument</h2>
+	<p>Vragen?<br>
+		<a href="mailto:klantenservice@azerty.nl">klantenservice@azerty.nl</a><br>
+        <a href="tel:0572328120">0572 328120</a></p>
+	<p>Service en garantie<br>
+		<a href="mailto:service@azerty.nl">service@azerty.nl</a></p>
+	<p>Betalingen en administratieve vragen<br>
+		<a href="mailto:administratie@azerty.nl">administratie@azerty.nl</a></p>
+		        
+        </div>
+
+        <div class="border flex-auto"> <p>Zakelijk</p>
+            <p>Vragen?<br>
+            <a href="mailto:verkoop@azerty.nl">verkoop@azerty.nl</a><br>
+            <a href="tel:0572328123">0572 328123</a></p>
+
+        </div>
+
+
+        <div class="border flex-auto">  <p>Sociale media</p>
+        <h2>Sociale Media</h2>
+
+            <p>Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/azerty.nl/">Azerty.nl</a><br>
+            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/azerty.nl/">azerty.nl</a><br>
+            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/azerty">azerty</a><br>
+            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/azerty_nl">@azerty_nl</a><br>
+            </p>
+        </div>
+
+>>>>>>> Stashed changes
     </div>
