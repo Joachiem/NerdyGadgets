63e02754abd6f54b98196b8e39954b008d93501b
Mart-0 <s1158005@student.windesheim.nl>
2020-11-20 10:42:42 +0100

    fix

--- partials/productcard.php
+++ partials/productcard.php
@@ -4,10 +4,14 @@
             <a href="/products/view?id=<?php print $product->StockItemID ?>">
                 <img class="w-full rounded-t p-1" src="/public/StockItemIMG/<?php print $product->ImagePath ?>">
             </a>
+
         </div>
 
         <div class="px-2 py-2 inset-x-0 bottom-0">
-            <div class="font-bold text-sm tracking-wide text-left"><?php print $product->StockItemName ?></div>
+            <a href="/products/view?id=<?php print $product->StockItemID ?>">
+                <div class="font-bold text-sm tracking-wide text-left"><?php print $product->StockItemName ?></div>
+            </a>
+
             <div class="flex flex-wrap grid grid-cols-3 gap-0">
                 <div class="col-span-2 text-gray-700 text-sm">€ <?php print round($product->SellPrice, 2) ?><p class="text-gray-500 text-xs">€ <?php print round($product->RecommendedRetailPrice, 2) ?> <?php print $GLOBALS['t']['tax'] ?></p>
                 </div>
