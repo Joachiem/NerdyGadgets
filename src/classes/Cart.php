d42885187371622c1b2a38d9abfba01d3318df90
Mart-0 <s1158005@student.windesheim.nl>
2020-11-22 22:09:10 +0100

    fix

--- src/classes/Cart.php
+++ src/classes/Cart.php
@@ -54,10 +54,11 @@ class Cart
     {
         $data = json_decode(file_get_contents('php://input'));
 
-        $_SESSION['cart'][$data->id] += 1;
+        if (isset($_SESSION['cart'][$data->id])) $_SESSION['cart'][$data->id] += 1;
+        else  $_SESSION['cart'][$data->id] = 1;
 
         http_response_code(201);
-        print json_encode(array('title' => $GLOBALS['t']['add-alert-title'], 'message' => $GLOBALS['t']['add-alert-message']));
+        print json_encode(['title' => $GLOBALS['t']['add-alert-title'], 'message' => $GLOBALS['t']['add-alert-message']]);
     }
 
 
