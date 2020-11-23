f36966db4b9c9776d57ba7a4310b8dd9984c28d2
Mart-0 <s1158005@student.windesheim.nl>
2020-11-23 10:46:37 +0100

    merge

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -2,11 +2,10 @@
 
 <div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
     <div class="border flex-1">
-        <p>Consument</p>
-
         <h2>Consument</h2>
 
-        <p>Vragen?<a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a>
+        <p>Vragen?
+            <a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a>
             <a href="tel:0572328120">0572 328120</a>
         </p>
 
@@ -15,22 +14,21 @@
     </div>
 
     <div class="border flex-auto">
-        <p>Zakelijk</p>
+        <h2>Zakelijk</h2>
         <p>Vragen?
-            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a><br>
-            <a href="tel:0572328123">0572 328123</a></p>
-
+            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a>
+            <a href="tel:0572328123">0572 328123</a>
+        </p>
     </div>
 
     <div class="border flex-auto">
-        <p>Sociale media</p>
         <h2>Sociale Media</h2>
 
-        <p>Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">.nl</a>
+        <p>
+            Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">.nl</a>
             Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">.nl</a>
             YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets"></a>
             Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@_nl</a>
         </p>
     </div>
-
 </div>
