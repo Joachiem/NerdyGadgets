821cf783041579235772ae98a62c996776fbbe51
MarkGortemaker <70703393+MarkGortemaker@users.noreply.github.com>
2020-11-23 14:34:00 +0100

    Update contact.php

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -2,19 +2,21 @@
 
 <h1 class="pb-12 text-3xl text-center font-extrabold">Contactinformatie </h1>
 
-<div class="flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
+<div class="flex flex-wrap grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
 
     <!-- Blok 1 -->
     <div class="flex-auto shadow-lg max-w-sm rounded material-card bg-white px-2 py-2 inset-x-0 bottom-0">
         <h2 class="pb-4 text-xl font-bold">Consument</h2>
 
         <p>
-            Klantenservice<br><a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
+            Klantenservice<br><a class="underline" 
+                href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
         </p>
 
-        <p><br>Service en garantie<br><a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a></p><br>
-        <p>Betalingen en administratieve<br><a
-                    href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p><br>
+        <p><br>Service en garantie<br><a class="underline" 
+                href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a></p><br>
+        <p>Betalingen en administratieve<br><a class="underline"
+                href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p><br>
         <p>Tel: 06-51423256</p>
     </div>
 
@@ -23,8 +25,9 @@
     <div class="flex-auto shadow-lg max-w-sm rounded material-card bg-white px-2 py-2 inset-x-0 bottom-0">
         <h2 class="pb-4 text-xl font-bold">Zakelijk</h2>
         <p>
-            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a>
-            <br><br>
+            <a class="underline" 
+                href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a><br>
+            <br>
         <p>Tel: 0900-8899</p>
 
         </p>
@@ -36,12 +39,14 @@
         <h2 class="pb-4 text-xl font-bold">Sociale Media</h2>
 
         <p>
-            Facebook: <a onclick="window.open(this.href); return false;"
-                         href="https://www.facebook.com/nerdygadgets.nl/">nerdygadgets</a><br>
-            Instagram: <a onclick="window.open(this.href); return false;"
-                          href="https://www.instagram.com/nerdygadgets.nl/">@nerdygadgets</a><br>
-            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets">nerdygadgets</a><br>
-            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@nerdygadgets</a><br>
+            Facebook: <a class="underline" onclick="window.open(this.href); return false;"
+                         href="https://www.facebook.com/nerdygadgets.nl/">Nerdygadgets</a><br>
+            Instagram: <a class="underline" onclick="window.open(this.href); return false;"
+                          href="https://www.instagram.com/nerdygadgets.nl/">@Nerdygadgets</a><br>
+            YouTube: <a class="underline" onclick="window.open(this.href); return false;" 
+                         href="https://www.youtube.com/nerdygadgets">Nerdygadgets</a><br>
+            Twitter: <a class="underline" onclick="window.open(this.href); return false;" 
+                         href="https://twitter.com/nerdygadgets">@Nerdygadgets</a><br>
         </p>
     </div>
 </div>
