1d04c7da6b7a2be992198caff3850b35a7369b37
Mart-0 <s1158005@student.windesheim.nl>
2020-11-23 10:44:51 +0100

    merge fix

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -1,44 +1,36 @@
-<?php ?>
-        <h1>Contactinformatie</h1>
+<h1>Contactinformatie</h1>
 
-<<<<<<< Updated upstream
-    <div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
-        <div class="border flex-1"> <p>Consument</p></div>
-        <div class="border flex-auto"> <p>Zakelijk</p></div>
-        <div class="border flex-auto"> <p>Sociale media</p></div>
-=======
-    <div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8"">
-        <div class="border flex-1"> <p>Consument</p>
-        
-      
-	<h2>Consument</h2>
-	<p>Vragen?<br>
-		<a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
-        <a href="tel:0572328120">0572 328120</a></p>
-	<p>Service en garantie<br>
-		<a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a></p>
-	<p>Betalingen en administratieve vragen<br>
-		<a href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p>
-		        
-        </div>
+<div class="border flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
+    <div class="border flex-1">
+        <p>Consument</p>
 
-        <div class="border flex-auto"> <p>Zakelijk</p>
-            <p>Vragen?<br>
-            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@aznerdygadgetserty.nl</a><br>
+        <h2>Consument</h2>
+
+        <p>Vragen?<a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a>
+            <a href="tel:0572328120">0572 328120</a>
+        </p>
+
+        <p>Service en garantie<a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a> </p>
+        <p>Betalingen en administratieve vragen<a href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p>
+    </div>
+
+    <div class="border flex-auto">
+        <p>Zakelijk</p>
+        <p>Vragen?
+            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a><br>
             <a href="tel:0572328123">0572 328123</a></p>
 
-        </div>
+    </div>
 
-
-        <div class="border flex-auto">  <p>Sociale media</p>
+    <div class="border flex-auto">
+        <p>Sociale media</p>
         <h2>Sociale Media</h2>
 
-            <p>Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">Azerty.nl</a><br>
-            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">azerty.nl</a><br>
-            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets">azerty</a><br>
-            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@azerty_nl</a><br>
-            </p>
-        </div>
-
->>>>>>> Stashed changes
+        <p>Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">.nl</a>
+            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">.nl</a>
+            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets"></a>
+            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@_nl</a>
+        </p>
     </div>
+
+</div>
