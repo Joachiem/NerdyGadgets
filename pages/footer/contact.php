16f692789b659ea4420ad6ab65f324a01cd021f4
Joachiem <j.m.veldkamp@hotmail.nl>
2020-11-23 14:23:46 +0100

    small changes

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -4,39 +4,42 @@
 
 <div class="flex mb-4 flex-wrap mx-1 lg:-mx-4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8">
 
-<!-- Blok 1 -->
+    <!-- Blok 1 -->
     <div class="flex-auto shadow-lg max-w-sm rounded material-card bg-white px-2 py-2 inset-x-0 bottom-0">
         <h2 class="pb-4 text-xl font-bold">Consument</h2>
 
         <p>
-            klantenservice<br><a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
-            
+            Klantenservice<br><a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
         </p>
 
         <p><br>Service en garantie<br><a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a></p><br>
-        <p>Betalingen en administratieve<br><a href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p><br>
-        <p>Tel: <a >0572 328120</a></p>
+        <p>Betalingen en administratieve<br><a
+                    href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p><br>
+        <p>Tel: 06-51423256</p>
     </div>
 
 
-<!-- Blok 2 -->
+    <!-- Blok 2 -->
     <div class="flex-auto shadow-lg max-w-sm rounded material-card bg-white px-2 py-2 inset-x-0 bottom-0">
         <h2 class="pb-4 text-xl font-bold">Zakelijk</h2>
         <p>
             <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a>
-            <br><br>Tel: <a >0572 328123</a><br>
-            
+            <br><br>
+        <p>Tel: 0900-8899</p>
+
         </p>
     </div>
 
 
-<!-- Blok 3 -->
+    <!-- Blok 3 -->
     <div class="flex-auto shadow-lg max-w-sm rounded material-card bg-white px-2 py-2 inset-x-0 bottom-0">
         <h2 class="pb-4 text-xl font-bold">Sociale Media</h2>
 
         <p>
-            Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">nerdygadgets</a><br>
-            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">@nerdygadgets</a><br>
+            Facebook: <a onclick="window.open(this.href); return false;"
+                         href="https://www.facebook.com/nerdygadgets.nl/">nerdygadgets</a><br>
+            Instagram: <a onclick="window.open(this.href); return false;"
+                          href="https://www.instagram.com/nerdygadgets.nl/">@nerdygadgets</a><br>
             YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets">nerdygadgets</a><br>
             Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@nerdygadgets</a><br>
         </p>
