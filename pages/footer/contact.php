12ddbadbdd0003315bbca1e03e2a5fb46aa9a606
RachedBataineh <70702863+RachedBataineh@users.noreply.github.com>
2020-11-23 13:38:55 +0100

    contact9

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -4,19 +4,19 @@
     <div class="border flex-1">
         <h2>Consument</h2>
 
-        <p>Vragen?
-            <a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a>
+        <p>
+            <a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
             <a href="tel:0572328120">0572 328120</a>
         </p>
 
-        <p>Service en garantie<a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a> </p>
-        <p>Betalingen en administratieve vragen<a href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p>
+        <p><br>Service en garantie<br><a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a></p><br>
+        <p>Betalingen en administratieve<br><a href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p>
     </div>
 
     <div class="border flex-auto">
         <h2>Zakelijk</h2>
-        <p>Vragen?
-            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a>
+        <p>
+            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@nerdygadgets.nl</a><br>
             <a href="tel:0572328123">0572 328123</a>
         </p>
     </div>
@@ -25,10 +25,10 @@
         <h2>Sociale Media</h2>
 
         <p>
-            Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">.nl</a>
-            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">.nl</a>
-            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets"></a>
-            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@_nl</a>
+            Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">nerdygadgets</a><br>
+            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">@nerdygadgets</a><br>
+            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets">nerdygadgets</a><br>
+            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@nerdygadgets</a><br>
         </p>
     </div>
 </div>
