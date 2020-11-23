2eae9eb7ef62990d60a9bb3891f97e7dbc4f6756
RachedBataineh <70702863+RachedBataineh@users.noreply.github.com>
2020-11-23 09:07:09 +0100

    contact8

--- pages/footer/contact.php
+++ pages/footer/contact.php
@@ -13,18 +13,18 @@
       
 	<h2>Consument</h2>
 	<p>Vragen?<br>
-		<a href="mailto:klantenservice@azerty.nl">klantenservice@azerty.nl</a><br>
+		<a href="mailto:klantenservice@nerdygadgets.nl">klantenservice@nerdygadgets.nl</a><br>
         <a href="tel:0572328120">0572 328120</a></p>
 	<p>Service en garantie<br>
-		<a href="mailto:service@azerty.nl">service@azerty.nl</a></p>
+		<a href="mailto:service@nerdygadgets.nl">service@nerdygadgets.nl</a></p>
 	<p>Betalingen en administratieve vragen<br>
-		<a href="mailto:administratie@azerty.nl">administratie@azerty.nl</a></p>
+		<a href="mailto:administratie@nerdygadgets.nl">administratie@nerdygadgets.nl</a></p>
 		        
         </div>
 
         <div class="border flex-auto"> <p>Zakelijk</p>
             <p>Vragen?<br>
-            <a href="mailto:verkoop@azerty.nl">verkoop@azerty.nl</a><br>
+            <a href="mailto:verkoop@nerdygadgets.nl">verkoop@aznerdygadgetserty.nl</a><br>
             <a href="tel:0572328123">0572 328123</a></p>
 
         </div>
@@ -33,10 +33,10 @@
         <div class="border flex-auto">  <p>Sociale media</p>
         <h2>Sociale Media</h2>
 
-            <p>Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/azerty.nl/">Azerty.nl</a><br>
-            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/azerty.nl/">azerty.nl</a><br>
-            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/azerty">azerty</a><br>
-            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/azerty_nl">@azerty_nl</a><br>
+            <p>Facebook: <a onclick="window.open(this.href); return false;" href="https://www.facebook.com/nerdygadgets.nl/">Azerty.nl</a><br>
+            Instagram: <a onclick="window.open(this.href); return false;" href="https://www.instagram.com/nerdygadgets.nl/">azerty.nl</a><br>
+            YouTube: <a onclick="window.open(this.href); return false;" href="https://www.youtube.com/nerdygadgets">azerty</a><br>
+            Twitter: <a onclick="window.open(this.href); return false;" href="https://twitter.com/nerdygadgets">@azerty_nl</a><br>
             </p>
         </div>
 
