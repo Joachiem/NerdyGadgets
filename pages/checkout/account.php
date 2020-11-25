74a25b981163cf8b85cf5672d4bf2a69eb47c496
kahnaksu1 <57707145+kahnaksu1@users.noreply.github.com>
2020-11-25 17:02:20 +0100

    EMAIIIILLLL

--- pages/checkout/account.php
+++ pages/checkout/account.php
@@ -118,7 +118,7 @@
                     <div class="w-full px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                for="grid-password">
-                               <?php print $GLOBALS['t']['mail'] ?>
+                               <?php print $GLOBALS['t']['email'] ?>
                         </label>
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->email) ? 'border-red-500' : 'border-gray-200' ?>"
                                name="email" id="grid-email" type="text"
