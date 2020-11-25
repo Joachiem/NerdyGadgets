2ae83d668cb0a67edbc35285def5652f68ac6b28
kahnaksu1 <57707145+kahnaksu1@users.noreply.github.com>
2020-11-25 11:13:57 +0100

    Vertaling

--- pages/checkout/account.php
+++ pages/checkout/account.php
@@ -12,7 +12,7 @@
                 </div>
             </div>
 
-            <div class="text-xs text-center text-gray-800 md:text-base">Gegevens</div>
+            <div class="text-xs text-center text-gray-800 md:text-base"><?php print $GLOBALS['t']['information'] ?></div>
         </div>
 
         <div class="w-1/4">
@@ -31,7 +31,7 @@
                 </div>
             </div>
 
-            <div class="text-xs text-center text-gray-800 md:text-base">Verzending</div>
+            <div class="text-xs text-center text-gray-800 md:text-base"><?php print $GLOBALS['t']['shipping'] ?></div>
         </div>
 
         <div class="w-1/4">
@@ -50,7 +50,7 @@
                 </div>
             </div>
 
-            <div class="text-xs text-center text-gray-800 md:text-base">Betalen</div>
+            <div class="text-xs text-center text-gray-800 md:text-base"><?php print $GLOBALS['t']['payment'] ?></div>
         </div>
 
         <div class="w-1/4">
@@ -69,7 +69,7 @@
                 </div>
             </div>
 
-            <div class="text-xs text-center text-gray-800 md:text-base">Afronden</div>
+            <div class="text-xs text-center text-gray-800 md:text-base"><?php print $GLOBALS['t']['overview'] ?></div>
         </div>
     </div>
 </div>
@@ -118,7 +118,7 @@
                     <div class="w-full px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                for="grid-password">
-                            E-mailadress
+                               <?php print $GLOBALS['t']['mail'] ?>
                         </label>
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->email) ? 'border-red-500' : 'border-gray-200' ?>"
                                name="email" id="grid-email" type="text"
@@ -137,7 +137,7 @@
                     <div class="w-full px-3">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                for="grid-password">
-                            Telefoonnummer
+                               <?php print $GLOBALS['t']['phonenumber'] ?>
                         </label>
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->phonenumber) ? 'border-red-500' : 'border-gray-200' ?>"
                                name="phonenumber" placeholder="06-12345678" id="grid-phonenumber" type="tel"
@@ -155,9 +155,9 @@
                 <div class="flex md:items-center justify-between">
                     <a href="/cart"
                        class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                       type="button">Terug</a>
+                       type="button"><?php print $GLOBALS['t']['back'] ?></a>
                     <input class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                           name="submit" type="submit" value="Doorgaan">
+                           name="submit" type="submit" value=<?php print $GLOBALS['t']['continue'] ?>>
                 </div>
             </form>
         </div>
