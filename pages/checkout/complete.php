f1fe2c331bbe3473f5450267071f1992241a4807
kahnaksu1 <57707145+kahnaksu1@users.noreply.github.com>
2020-12-01 14:16:12 +0100

    Update complete.php

--- pages/checkout/complete.php
+++ pages/checkout/complete.php
@@ -14,8 +14,7 @@
 
         <div class="w-1/4">
             <div class="relative mb-2">
-                <div class="absolute flex align-center items-center align-middle content-center"
-                     style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
+                <div class="absolute flex align-center items-center align-middle content-center" style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
                     <div class="w-full bg-white rounded items-center align-middle align-center flex-1">
                         <div class="w-0 bg-green-300 py-1 rounded" style="width: 100%;"></div>
                     </div>
@@ -33,8 +32,7 @@
 
         <div class="w-1/4">
             <div class="relative mb-2">
-                <div class="absolute flex align-center items-center align-middle content-center"
-                     style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
+                <div class="absolute flex align-center items-center align-middle content-center" style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
                     <div class="w-full bg-white rounded items-center align-middle align-center flex-1">
                         <div class="w-0 bg-green-300 py-1 rounded" style="width: 100%;"></div>
                     </div>
@@ -52,8 +50,7 @@
 
         <div class="w-1/4">
             <div class="relative mb-2">
-                <div class="absolute flex align-center items-center align-middle content-center"
-                     style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
+                <div class="absolute flex align-center items-center align-middle content-center" style="width: calc(100% - 2.5rem - 1rem); top: 50%; transform: translate(-50%, -50%)">
                     <div class="w-full bg-white rounded items-center align-middle align-center flex-1">
                         <div class="w-0 bg-green-300 py-1 rounded" style="width: 100%;"></div>
                     </div>
@@ -71,62 +68,64 @@
     </div>
 </div>
 
+<!-- 
+<div class= "flex justify-center" >
+   </div> -->
 
 <div class="flex justify-center">
     <div class="overflow-hidden rounded-lg shadow-lg mb-8 max-w-xl">
 
         <div class="leading-tight p-2 md:p-4 bg-white">
             <form class="w-full max-w-lg">
-
+                <div class="flex justify-center pb-4">
+                    <img class="rounded object-cover" id="logo" draggable="false" src="/public/img/logo.svg" />
+                </div>
                 <div class="justify-around text-xl flex w-full flex-wrap mb-6">
                     <h2><?php print $GLOBALS['t']['payment-done'] ?></h2>
                 </div>
                 <div>
-                    <table class="w-full text-sm lg:text-base" cellspacing="0">
+                    <table class="w-full table-fixed text-sm lg:text-base" cellspacing="0">
                         <thead>
-                        <tr class="h-12 uppercase">
-                            <th class="hidden md:table-cell"></th>
-                            <th class="text-left"><?php print $GLOBALS['t']['products'] ?></th>
-                            <th class="lg:text-right text-left pl-5 lg:pl-0">
-                                <span class="lg:hidden" title="Quantity">Ant.</span>
-                                <span class="hidden lg:inline"><?php print $GLOBALS['t']['quantity'] ?></span>
-                            </th>
-                            <th class="hidden text-right md:table-cell"><?php print $GLOBALS['t']['price'] ?></th>
-                        </tr>
+                            <tr class="h-12 uppercase">
+                                <th class=" md:table-cell"></th>
+                                <th class="text-left"><?php print $GLOBALS['t']['products'] ?></th>
+                                <th class="text-left pl-5 lg:pl-0">
+                                    <span class="lg:hidden" title="Quantity">Ant.</span>
+                                    <span class="hidden lg:inline"><?php print $GLOBALS['t']['quantity'] ?></span>
+                                </th>
+                                <th class="text-right md:table-cell"><?php print $GLOBALS['t']['price'] ?></th>
+                            </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($arg as $id => $product_obj) { ?>
+                            <?php foreach ($arg as $id => $product_obj) { ?>
 
-                            <?php $Images = $product_obj['images'] ?>
-                            <?php $Result = $product_obj['product'] ?>
+                                <?php $Images = $product_obj['images'] ?>
+                                <?php $Result = $product_obj['product'] ?>
 
-                            <tr class="cart-items" id="row-<?php print $id; ?>">
-                                <td class="hidden pb-4 md:table-cell">
-                                    <img src="/public/StockItemIMG/<?php print $Images[0]->ImagePath; ?>"
-                                         class="w-20 p-1 rounded" alt="Thumbnail">
-                                </td>
-                                <td>
-                                    <a href="/products/view?id=<?php print $id; ?>">
-                                        <p class="mb-2 md:ml-4"><?php print $Result[0]->StockItemName; ?></p>
-                                    </a>
-                                </td>
-                                <td><?php print($product_obj['qty']) ?></td>
-                                <td class="hidden text-right md:table-cell">
-                                <span class="text-sm lg:text-base font-medium"> €
-                                    <span id="price-<?php print($id) ?>">
-                                        <?php print sprintf("%.2f", $Result[0]->SellPrice); ?>
-                                    </span>
-                                </span>
-                                </td>
-                            </tr>
-                        <?php } ?>
+                                <tr class="cart-items" id="row-<?php print $id; ?>">
+                                    <td class="pb-4 md:table-cell">
+                                        <img src="/public/StockItemIMG/<?php print $Images[0]->ImagePath; ?>" class="w-20 p-1 rounded" alt="Thumbnail">
+                                    </td>
+                                    <td class= "text-center">
+                                        <a href="/products/view?id=<?php print $id; ?>">
+                                            <p class="mb-2 md:ml-4"><?php print $Result[0]->StockItemName; ?></p>
+                                        </a>
+                                    </td>
+                                    <td class=" pb-4 md:table-cell"><?php print($product_obj['qty']) ?></td>
+                                    <td class=" text-right md:table-cell">
+                                        <span class="text-sm lg:text-base font-medium"> €
+                                            <span id="price-<?php print($id) ?>">
+                                                <?php print sprintf("%.2f", $Result[0]->SellPrice); ?>
+                                            </span>
+                                        </span>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
                 <div class="flex md:items-center justify-around">
-                    <a href="/"
-                       class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                       type="button"><?php print $GLOBALS['t']['continue'] ?></a>
+                    <a href="/" class="shadow bg-teal-400 hover:bg-teal-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button"><?php print $GLOBALS['t']['continue'] ?></a>
                 </div>
             </form>
         </div>
