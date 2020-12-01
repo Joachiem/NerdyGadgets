dca1bbdb66dafc5d7cec82baf728f9ba98475d04
RachedBataineh <70702863+RachedBataineh@users.noreply.github.com>
2020-12-01 16:58:11 +0100

    UPDATE

--- pages/user/profile.php
+++ pages/user/profile.php
@@ -1,10 +1,10 @@
 <?php
-print("Welcome to your profile");
+print("Welcome to your profile "); print_r($_SESSION['user']['0']);  
 print('<pre>');
-print_r($_SESSION['user']);
+//print_r($_SESSION['user']['0']);
 
 ?>
 
 <form action="/logout" method="POST">
-    <input type="submit" value="logout">
+    <input class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit" value="logout">
 </form>
