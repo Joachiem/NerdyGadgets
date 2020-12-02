0d873a20f55a36a818a352e4970bd41d48a26895
Mart-0 <s1158005@student.windesheim.nl>
2020-12-02 09:35:23 +0100

    sd

--- pages/user/login.php
+++ pages/user/login.php
@@ -7,7 +7,7 @@
 
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                            <?php print $GLOBALS['t']['username'] ?>
+                            <?php print $GLOBALS['t']['email'] ?>
                         </label>
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value="<?php isset($_SESSION['login']["email"]) ? print($_SESSION['login']["email"]) : '' ?>" name="email" id="email" type="email" placeholder="email">
                     </div>
