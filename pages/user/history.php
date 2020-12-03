324d4616aa58de4f99e39eda548724801333818e
MarkGortemaker <70703393+MarkGortemaker@users.noreply.github.com>
2020-12-03 15:11:06 +0100

    Update history.php

--- pages/user/history.php
+++ pages/user/history.php
@@ -1,15 +1,83 @@
 <article class="overflow-hidden rounded-lg shadow-lg bg-white">
-    <a href="/products?category_id=<?php print $StockGroup->StockGroupID ?>">
-        <div>
-            <img alt="Placeholder" class="block object-cover h-48 w-full" src="public/StockGroupIMG/<?php print $StockGroup->ImagePath ?>">
-        </div>
-    </a>
+    <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y rounded">
+        <table class="w-full text-sm lg:text-base table-auto" cellspacing="0">
+            <thead>
+                <tr class="h-12 uppercase">
+                    <th class="text-left">Order-ID</th>
+                    <th class="text-left"><?php print $GLOBALS['t']['products'] ?></th>
+                    <th class="lg:text-right text-left pl-5 lg:pl-0">
+                        <span class="lg:inline">Qty<?php //print $GLOBALS['t']['amount'] 
+                                                    ?></span>
+                    </th>
+                    <th class="text-right"> <?php print $GLOBALS['t']['price'] ?></th>
+                </tr>
+            </thead>
+            <tbody>
 
-    <div class="flex items-center justify-between leading-tight p-2 md:p-4">
-        <h1 class="text-lg">
-            <a class="no-underline hover:underline text-black" href="/products?category_id=<?php print $StockGroup->StockGroupID ?>">
-                <?php print $StockGroup->StockGroupName ?>
-            </a>
-        </h1>
+
+
+
+                <?php foreach ($arg->orders as $order) { ?>
+
+                    <tr class="border-b-2 ">
+                        <td class="pb-4 md:table-cell">
+                            <a href="/products/view?id=<?php print $order->OrderID
+                                                        ?>"><?php print $order->OrderID ?>
+
+                            </a>
+                        </td>
+
+
+                        <td class="pb-4 pt-1">
+                            <ul>
+                                <?php foreach ($order->lines as $lines) {
+                                ?>
+                                    <li> <a href="/products/view?id=<?php print $lines->StockItemID ?> " target="_blank">
+                                            <?php print_r($lines->Description); ?>
+                                        </a>
+                                    </li>
+
+                                <?php } ?>
+                            </ul>
+
+
+                        </td>
+
+                        <td class="pb-4 md:table-cell m-4 text-right ">
+                            <ul>
+                                <?php foreach ($order->lines as $lines) {
+                                ?>
+                                    <li> <?php print_r($lines->Quantity); ?>
+                                    </li>
+
+                                <?php } ?>
+                            </ul>
+
+
+                        </td>
+
+
+                        <td class="pb-4 md:table-cell m-4 text-right ">
+                            <ul>
+                                <?php foreach ($order->lines as $lines) {
+                                ?>
+                                    <li> <?php print_r($lines->UnitPrice); ?>
+                                    </li>
+
+                                <?php } ?>
+                            </ul>
+
+
+                        </td>
+
+
+                    </tr>
+
+                <?php } ?>
+
+            </tbody>
+        </table>
+
+
     </div>
 </article>
