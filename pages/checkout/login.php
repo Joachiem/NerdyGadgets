3dca93b38cabd7affb8c168f2eed3f63faf41a5d
camiël vd Niet <35525009+camvdniet@users.noreply.github.com>
2020-12-09 10:19:57 +0100

    Update login.php

--- pages/checkout/login.php
+++ pages/checkout/login.php
@@ -20,7 +20,7 @@
                 <form action method="POST">
                     <h1 class="mb-2">Login</h1>
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email"> Email </label>
-                    <input type="email"  class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->firstname) ? 'border-red-500' : 'border-gray-200' ?>"
+                    <input type="email"  class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->email) ? 'border-red-500' : 'border-gray-200' ?>"
                         name="email" id="email" type="text"
                         value="<?php isset($_SESSION['form']["email"]) ? print($_SESSION['form']["email"]) : '' ?>">
 
@@ -32,7 +32,7 @@
                 <?php } ?>
 
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password"> <?php print $GLOBALS['t']['password'] ?> </label>
-                    <input type="password" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->firstname) ? 'border-red-500' : 'border-gray-200' ?>"
+                    <input type="password" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white <?php print isset($error_messages->password) ? 'border-red-500' : 'border-gray-200' ?>"
                         name="password" id="password" type="text">
 
                     <?php if (isset($error_messages->password)) { ?>
