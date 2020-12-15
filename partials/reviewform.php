02cde4a74e5690cb769264e916c864c43d80d163
camiël vd Niet <35525009+camvdniet@users.noreply.github.com>
2020-12-15 13:58:45 +0100

    Update reviewform.php

--- partials/reviewform.php
+++ partials/reviewform.php
@@ -2,11 +2,11 @@
 
 <form method="POST" class="w-full w-full">
     <fieldset name="stars" class="rating flex">
-        <input type="radio" id="star1" name="rating" value="1" checked /><label class="full" for="star1" title="1 star"></label>
-        <input type="radio" id="star2" name="rating" value="2" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '2' ? 'checked' : '' : '' ?> /><label class="full" for="star2" title="2 stars"></label>
-        <input type="radio" id="star3" name="rating" value="3" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '3' ? 'checked' : '' : '' ?> /><label class="full" for="star3" title="3 stars"></label>
-        <input type="radio" id="star4" name="rating" value="4" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '4' ? 'checked' : '' : '' ?> /><label class="full" for="star4" title="4 stars"></label>
-        <input type="radio" id="star5" name="rating" value="5" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '5' ? 'checked' : '' : '' ?> /><label class="full" for="star5" title="5 stars"></label>
+        <span class="star"><input type="radio" class="starbtn" id="star1" name="rating" value="1" checked /><label class="full" for="star1" title="1 star"></label></span>
+        <span class="star"><input type="radio" class="starbtn" id="star2" name="rating" value="2" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '2' ? 'checked' : '' : '' ?> /><label class="full" for="star2" title="2 stars"></label></span>
+        <span class="star"><input type="radio" class="starbtn" id="star3" name="rating" value="3" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '3' ? 'checked' : '' : '' ?> /><label class="full" for="star3" title="3 stars"></label></span>
+        <span class="star"><input type="radio" class="starbtn" id="star4" name="rating" value="4" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '4' ? 'checked' : '' : '' ?> /><label class="full" for="star4" title="4 stars"></label></span>
+        <span class="star"><input type="radio" class="starbtn" id="star5" name="rating" value="5" <?php isset($_SESSION['review']['form']['rating']) ? print $_SESSION['review']['form']['rating'] === '5' ? 'checked' : '' : '' ?> /><label class="full" for="star5" title="5 stars"></label></span>
     </fieldset>
 
     <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
@@ -37,3 +37,18 @@
 
     <input class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit" value="Submit Review">
 </form>
+
+<style>
+.starbnt {
+    content: '\f005  ';
+}
+
+.star {
+    background-color: yellow;
+}
+
+.star:hover ~ .star {
+    background-color: gray;
+}
+
+</style>
