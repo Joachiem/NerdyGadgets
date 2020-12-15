b4ab360c6e2cf5de94250586462f23295b14098f
Mart-0 <s1158005@student.windesheim.nl>
2020-12-15 10:40:38 +0100

    fix

--- doc/db-changes.sql
+++ doc/db-changes.sql
@@ -8,7 +8,8 @@ DEFAULT (0)
 UPDATE `specialdeals` SET `StockItemID` = '46' WHERE `specialdeals`.`SpecialDealID` = 2;
 UPDATE `specialdeals` SET `StockItemID` = '2' WHERE `specialdeals`.`SpecialDealID` = 1;
 
-CREATE TABLE `nerdygadgets`.`peopleaddress` 
+DROP TABLE IF EXISTS peopleaddress;
+CREATE TABLE peopleaddress
 (
     `addresid` INT(14) NOT NULL AUTO_INCREMENT,
     `peopleid` INT(14) NOT NULL,
@@ -20,20 +21,21 @@ CREATE TABLE `nerdygadgets`.`peopleaddress`
 ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS discount_codes;
-
 CREATE TABLE discount_codes (
     discount_code varchar(25) NOT NULL,
     discount int NOT NULL,
     expire date NOT NULL,
     primary key (discount_code)
-); 
+)
+ENGINE = InnoDB; 
 
-INSERT INTO `discount_codes` (`discount_code`, `discount`, `expire`) VALUES ('50off', '50', '2021-12-31');
+INSERT INTO `discount_codes` (`discount_code`, `discount`, `expire`) VALUES ('50off', '50', '2022-12-31');
 
 ALTER TABLE customers DROP CONSTRAINT FK_Sales_Customers_BillToCustomerID_Sales_Customers
 ALTER TABLE orders DROP CONSTRAINT FK_Sales_Orders_CustomerID_Sales_Customers
 
-CREATE TABLE nerdygadgets.reviews
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews
 (
     ReviewID INT(11) NOT NULL AUTO_INCREMENT,
     StockItemID INT(11) NOT NULL,
@@ -43,4 +45,5 @@ CREATE TABLE nerdygadgets.reviews
     Review MEDIUMTEXT NULL,
     Date DATE NOT NULL,
     PRIMARY KEY (reviewID)
-);
+)
+ENGINE = InnoDB;
