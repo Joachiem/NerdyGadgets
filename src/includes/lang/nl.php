415072f4d195df4cdcc48f586797bcfb5e59cc9c
Mart-0 <s1158005@student.windesheim.nl>
2020-12-15 13:40:33 +0100

    fix

--- src/includes/lang/nl.php
+++ src/includes/lang/nl.php
@@ -149,6 +149,7 @@ $GLOBALS['t'] = [
     'format-email' => 'Niet gedige email',
     'password-length' => 'Het wachtwoord moet minimaal 8 karakters lang zijn',
     'username-length' => 'De gebruikersnaam moet minimaal 8 karakters lang zijn',
+    'phonenumber-length' => 'het telefoonnummer moet minimaal 8 karakters lang zijn',
     'emailorpasswordwrong' => 'E-mail en/of wachtwoord zijn onjuist!',
     'email-taken' => 'Deze email is bezet',
 
